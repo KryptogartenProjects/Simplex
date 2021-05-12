c02cee0d9da5174691e55a7112f7a8cfef68b415
torjc01 <julio_cesar.torres@sct.gouv.qc.ca>
2021-05-12 17:00:56 -0400

    Initial commit

---
+++ simplexApi/index.js
@@ -0,0 +1,17 @@
+let express = require('express'); 
+let app = express(); 
+require('dotenv').config();  
+
+let router = express.Router(); 
+const PORT = process.env.PORT;
+const APP_NAME = process.env.APP_NAME; 
+
+router.get('/', function(req, res, next){
+    res.status(200).send("Lemon");
+});
+
+app.use('/api/', router); 
+
+var server = app.listen(PORT, function(){
+    console.log(APP_NAME + " is running on http://localhost:" + PORT); 
+});
